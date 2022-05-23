0bd4c8a3f4d86c059f797a35e5d7b0028682a51c
Braejan Arias <barias@sonatype.com>
2022-05-23 17:11:16 -0500

    :white_check_mark: remove onlyOwner from mintTickets

--- contracts/OpenTicket.sol
+++ contracts/OpenTicket.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OpenTicket is ERC1155, Ownable{
+contract OpenTicket is ERC1155, Ownable {
     uint256 public constant TICKET = 0;
 
     constructor(uint256 totalSupply, string memory uri_) ERC1155(uri_) 
