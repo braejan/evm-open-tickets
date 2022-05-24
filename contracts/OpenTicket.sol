98e4b3881ec0f0d92bc2040032c0c4f093f401f5
Braejan Arias <barias@sonatype.com>
2022-05-24 14:14:49 -0500

    :construction: unit test for deploy and mint tickets.

--- contracts/OpenTicket.sol
+++ contracts/OpenTicket.sol
@@ -6,19 +6,76 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OpenTicket is ERC1155, Ownable {
-    uint256 public constant TICKET = 0;
+    uint256[] public ALLOWED_TICKETS= [NORMAL_TICKET, VIP_TICKET, PREMIUM_TICKET];
+    uint256 NORMAL_TICKET = 0;
+    uint256 VIP_TICKET = 1;
+    uint256 PREMIUM_TICKET = 2;
+    uint256[] TICKETS_PRICE = [0 wei, 0 wei, 0 wei];
+    uint256[] TICKETS_SUPPLY = [0, 0, 0];
+    constructor(string memory uri_) ERC1155(uri_) {}
 
-    constructor(uint256 totalSupply, string memory uri_) ERC1155(uri_) 
+    function mintTickets(
+        uint256 ID,
+        uint256 supply,
+        uint256 price
+    ) public notInit(ID) onlyOwner {
+        TICKETS_SUPPLY[ID] = supply;
+        TICKETS_PRICE[ID] = price;
+        _mint(msg.sender, ID, supply, "0x000");
+    }
+
+    function ticketPrice(uint256 ID)
+        public
+        view
+        notAllowedID(ID)
+        returns(uint256)
     {
-        mintTickets(totalSupply);
+     return TICKETS_PRICE[ID];   
     }
 
-    function mintTickets(uint256 supply) public notInit(TICKET) {
-        _mint(msg.sender, TICKET, supply, "0x000");
+    function buyTicket(uint256 ID, uint256 amount) 
+       public
+       payable
+       notEnoughEthers(ID, amount)
+       notEnoughSupply(ID, amount)
+    {
+        _safeTransferFrom(address(owner()), msg.sender, ID, amount, "0x00");
+        TICKETS_SUPPLY[ID] =  TICKETS_SUPPLY[ID] - amount;
     }
 
-    modifier notInit(uint256 ID){
-        require(balanceOf(msg.sender, ID) == 0, "you already minted tickets");
+    function withdraw() public hasBalance onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    modifier notInit(uint256 ID) {
+        require(isAllowedID(ID), "ID not allowed");
+        require(TICKETS_SUPPLY[ID] == 0, "you already minted tickets for this ID");
         _;
+    }
+
+    modifier notAllowedID(uint256 ID) {
+        require(isAllowedID(ID), "ID not allowed");
+        _;
+    }
+
+    modifier notEnoughEthers(uint256 ID, uint256 amount) {
+        require(isAllowedID(ID), "ID not allowed");
+        require(msg.value >= amount * TICKETS_PRICE[ID]);
+        _;
+    }
+
+    modifier notEnoughSupply(uint256 ID, uint256 amount) {
+        require(isAllowedID(ID), "ID not allowed");
+        require(TICKETS_SUPPLY[ID] >= amount, "not enough supply for sent ID");
+        _;
+    }
+
+    modifier hasBalance {
+        require(address(this).balance > 0, "not enough balance. balance is 0");
+        _;
+    }
+
+    function isAllowedID(uint256 ID) private view returns(bool){
+        return ID >= 0 && ID <= ALLOWED_TICKETS.length;
     }
 }
--- test/OpenTicket.js
+++ test/OpenTicket.js
@@ -1,26 +1,57 @@
 const{ expect } = require("chai");
 const{ ethers } = require("hardhat");
 const TICKET = 0;
-
+const NORMAL_TICKET = 0;
+const VIP_TICKET = 1;
+const PREMIUM_TICKET = 2;
 describe("Open Tickets Tests", function() {
     let availableSigners;
     let deployer;
     let OpenTicket;
     let openTicketContract;
-    const initialSupply = 500;
-    const uri_ = "https://gateway.pinata.cloud/ipfs/QmTN32qBKYqnyvatqfnU8ra6cYUGNxpYziSddCatEmopLR/metadata/api/item/{id}.json";
+    const uri_ = "https://github.com/braejan/evm-open-tickets";
     before(async function () {
         availableSigners = await ethers.getSigners();
         deployer = availableSigners[0];
         OpenTicket = await ethers.getContractFactory("OpenTicket");
-        openTicketContract = await OpenTicket.deploy(initialSupply, uri_);
+        openTicketContract = await OpenTicket.deploy(uri_);
         await openTicketContract.deployed();
     });
 
-    it("Should mint tickets", async function() {
-        const balance = await openTicketContract.balanceOf(deployer.address, TICKET);
-        const contractUri = await openTicketContract.uri(TICKET);
-        expect(initialSupply).to.be.equal(Number(balance.toString()));
-        expect(uri_).to.be.equal(contractUri);
-    })
+    it("Should be in zero values for all ID", async function() {
+        //Given: a OpenTicket contract that is just deployed
+        //When: check allowed tickets
+        //Then: 
+        //  expect allowed tickets zero values
+        const [price1, price2, price3] = await Promise.all(
+            [
+                openTicketContract.ticketPrice(ethers.BigNumber.from(NORMAL_TICKET)),
+                openTicketContract.ticketPrice(ethers.BigNumber.from(VIP_TICKET)),
+                openTicketContract.ticketPrice(ethers.BigNumber.from(PREMIUM_TICKET))
+            ]
+        );
+        expect(0).to.be.equal(price1);
+        expect(0).to.be.equal(price2);
+        expect(0).to.be.equal(price3);
+    });
+
+    it("Should mint correctly", async function() {
+        await Promise.all(
+            [
+                openTicketContract.mintTickets(NORMAL_TICKET, 1500, 25000000000000000n),
+                openTicketContract.mintTickets(VIP_TICKET, 500, 40000000000000000n),
+                openTicketContract.mintTickets(PREMIUM_TICKET, 50, 75000000000000000n)
+            ]
+        );
+        const [price1, price2, price3] = await Promise.all(
+            [
+                openTicketContract.ticketPrice(ethers.BigNumber.from(NORMAL_TICKET)),
+                openTicketContract.ticketPrice(ethers.BigNumber.from(VIP_TICKET)),
+                openTicketContract.ticketPrice(ethers.BigNumber.from(PREMIUM_TICKET))
+            ]
+        );
+        expect(25000000000000000n).to.be.equal(price1);
+        expect(40000000000000000n).to.be.equal(price2);
+        expect(75000000000000000n).to.be.equal(price3);
+    });
 });
